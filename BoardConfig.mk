46d1fc7f9379d2249ffcb26d1ff29e7e6102f449
kyasu <kyasu.dev@gmail.com>
2018-10-10 00:48:58 +0900

    klimtdcm: Set SDK API level for rild
    * P made changes to pthread_mutex_init() that are incompatible
      with our libsec-ril libraries. In order for rild to even see a
      radio as being availble we need the legacy behavior.
    * Even though our libsec-ril libraries are from 5.0, which is API
      level 21, we do not appear to require any of the other behaviors
      from API levels earlier than 27.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -69,7 +69,8 @@ TARGET_LD_SHIM_LIBS += \
 TARGET_PROCESS_SDK_VERSION_OVERRIDE += \
     /system/bin/mediaserver=22 \
     /system/vendor/bin/hw/android.hardware.sensors@1.0-service.samsung8974=22 \
-    /system/vendor/bin/mm-qcamera-daemon=22
+    /system/vendor/bin/mm-qcamera-daemon=22 \
+    /system/vendor/bin/hw/rild=27
 
 # Lights
 TARGET_PROVIDES_LIBLIGHT := true
