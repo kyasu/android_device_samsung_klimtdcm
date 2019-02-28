a871eb0ae8c607f04aed6e79467d40b6db18d0b3
kyasu <kyasu.dev@gmail.com>
2019-02-28 15:17:28 +0900

    klimtdcm: Set default hotspot name
    * Make default bluetooth name same

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -17,7 +17,7 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME   "Galaxy Tab S 8.4 (SC-03G)"
+#define BTM_DEF_LOCAL_NAME   "Galaxy Tab S 8.4"
 
 #define BTA_DISABLE_DELAY 100 /* in milliseconds */
 #define BLE_VND_INCLUDED TRUE
