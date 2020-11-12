1baf85ad359ff3ba622ecaac6d4ef6578a373bb4
kyasu <kyasu.dev@gmail.com>
2020-11-13 08:26:56 +0900

    klimtdcm: msm8974-common: Do not use Snap
    * Use default Camera2.

--- msm8974-common/msm8974.mk
+++ msm8974-common/msm8974.mk
@@ -89,8 +89,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     camera.device@1.0-impl \
     camera.msm8974 \
-    libxml2 \
-    Snap
+    libxml2
 
 # DRM
 PRODUCT_PACKAGES += \
